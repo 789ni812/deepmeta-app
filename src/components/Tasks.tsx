10402d424634e08ed646f54bc0069d8169cd3f93
Stuart Bradford <5435563+789ni812@users.noreply.github.com>
2024-01-16 10:09:33 +0000

    Fix-up-typescript-issues
    * fix up typescript
    
    * fix up typescript
    
    * fix up typescript

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,23 +1,85 @@
-
+import React, { useEffect, useState } from 'react';
 import { api } from "~/utils/api";
-import { Task } from "./Task";
+
+import {
+  closestCenter,
+  DndContext,
+  DragOverlay,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+  type DragStartEvent,
+  type DragEndEvent,
+} from '@dnd-kit/core';
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable';
+import Item from './ItemToSort';
+
+import { type Task as TaskType } from "../types"; 
+import { Task as TaskComponent } from './Task';
 
 export function Tasks() {
     const { data: tasks, isLoading, isError } = api.task.all.useQuery();
+    const { mutate: updateTaskOrder } = api.task.updateOrder.useMutation();
 
-    if (isLoading) return <div>Loading...</div>
-	if (isError) return <div>Error fetching tasks</div>
-	
+    const [activeId, setActiveId] = useState<number | null>(null); 
+    const [localTasks, setLocalTasks] = useState<TaskType[]>([]); 
 
-    console.log("Tasks: ", tasks)
-	return (
-		<>
-<div>tasks</div>        
-			{tasks.length ?
-				tasks.map((task) => {
-                    return <Task key={task.id} task={task} />
-				})
-				: "Create a task"}
-		</>
-	)
+    useEffect(() => {
+      if (tasks) {
+        setLocalTasks([...tasks].sort((a, b) => a.order - b.order)); 
+      }
+    }, [tasks]);
+
+    function handleDragStart(event: DragStartEvent) { 
+        const { active } = event;
+        setActiveId(active.id as number);
+    }
+
+    function handleDragEnd(event: DragEndEvent) { 
+        const { active, over } = event;
+        if (over && active.id !== over.id) {
+            setLocalTasks(currentItems => {
+                const oldIndex = currentItems.findIndex((item) => item.id === active.id);
+                const newIndex = currentItems.findIndex(item => item.id === over.id);
+                const reorderedItems = arrayMove(currentItems, oldIndex, newIndex);
+                const updatedOrder = reorderedItems.map((item, index) => ({
+                    id: item.id,
+                    order: index
+                }));
+                
+                updateTaskOrder(updatedOrder);
+                return [...reorderedItems];
+            });
+        }
+        setActiveId(null);
+    }
+
+    const sensors = useSensors(
+        useSensor(PointerSensor),
+        useSensor(KeyboardSensor, {
+            coordinateGetter: sortableKeyboardCoordinates,
+        })
+    );
+
+    if (isLoading) return <div>Loading...</div>;
+    if (isError) return <div>Error fetching tasks</div>;
+
+    return (
+        <DndContext sensors={sensors} collisionDetection={closestCenter} onDragStart={handleDragStart} onDragEnd={handleDragEnd}>
+            <SortableContext items={localTasks.map(task => task.id)} strategy={verticalListSortingStrategy}>
+                {localTasks.map(task => (
+                    <TaskComponent key={task.id} task={task} setLocalTasks={setLocalTasks} />
+                ))}
+            </SortableContext>
+            <DragOverlay>
+                {activeId ? <Item id={activeId.toString()} /> : null}
+            </DragOverlay>
+        </DndContext>
+    );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,22 @@
 import { CreateTask } from "~/components/CreateTask";
 import { Tasks } from "~/components/Tasks";
-import {TestSorting} from "../components/TestSorting"
+
 
 
 export default function Home() {
-  return (
-    <>
-      <main className="m-4">
-        {/* YOU CAN REMOVE CODE FROM HERE */}
-        <div className="flex h-full w-full flex-col items-center justify-center">
-          <h1 className="text-xl font-medium">
-          Techly.ai
-          </h1>
-
-{/* <Tasks /> */}
-<CreateTask />
-<TestSorting />
-         </div>
-      </main>
-    </>
-  );
+    return (
+        <>
+            <main className="m-4">
+                <div className="flex h-full w-full flex-col items-center justify-center">
+                    <h1 className="text-xl font-medium">
+                        Techly.ai
+                    </h1>
+                    <div className="my-6">
+                    <CreateTask />
+                    </div>
+                    <Tasks />
+                </div>
+            </main>
+        </>
+    );
 }
