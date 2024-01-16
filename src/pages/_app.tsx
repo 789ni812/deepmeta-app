cd1291844cdcd744181059c21e247e3a07fdb490
789ni812 <Bradfordstuart@gmail.com>
2024-01-16 10:28:55 +0000

    add toast messaging

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import Layout from "~/components/layout";
+import { Toaster } from "react-hot-toast";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <>
       <Layout>
         <Component {...pageProps} />
+        <Toaster />
       </Layout>
     </>
   );
