357cb6e821abce1bf0433822c199582f2fd5b7ef
789ni812 <Bradfordstuart@gmail.com>
2024-01-16 10:11:15 +0000

    remove redundant code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,20 @@
 import { CreateTask } from "~/components/CreateTask";
 import { Tasks } from "~/components/Tasks";
-import {TestSorting} from "../components/TestSorting"
+import { TestSorting } from "../components/TestSorting"
 
 
 export default function Home() {
-  return (
-    <>
-      <main className="m-4">
-        {/* YOU CAN REMOVE CODE FROM HERE */}
-        <div className="flex h-full w-full flex-col items-center justify-center">
-          <h1 className="text-xl font-medium">
-          Techly.ai
-          </h1>
-
-{/* <Tasks /> */}
-<CreateTask />
-<TestSorting />
-         </div>
-      </main>
-    </>
-  );
+    return (
+        <>
+            <main className="m-4">
+                <div className="flex h-full w-full flex-col items-center justify-center">
+                    <h1 className="text-xl font-medium">
+                        Techly.ai
+                    </h1>
+                    <CreateTask />
+                    <TestSorting />
+                </div>
+            </main>
+        </>
+    );
 }
